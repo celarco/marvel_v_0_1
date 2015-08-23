9676614878539601bc7616d5624ae88bb7debb9f
TIR4N4Z0R05 <mhkazemi_engineer@yahoo.com>
2015-08-23 10:54:56 -0400

    fix video rotation problem

--- src/window_detector.cpp
+++ src/window_detector.cpp
@@ -21,6 +21,70 @@ double some=0;
  using namespace cv;
  using namespace std;
 
+
+// Return the rotation matrices for each rotation
+void rotate(cv::Mat& src, double angle, cv::Mat& dst) {
+  cv::Mat r = getRotationMatrix2D(cv::Point2f(), angle, 1.0);
+
+  //4 coordinates of the image
+  std::vector<cv::Point2f> corners(4);
+  corners[0] = cv::Point2f(0, 0);
+  corners[1] = cv::Point2f(0, src.rows);
+  corners[2] = cv::Point2f(src.cols, 0);
+  corners[3] = cv::Point2f(src.cols, src.rows);
+
+  std::vector<cv::Point2f> cornersTransform(4);
+  cv::transform(corners, cornersTransform, r);
+
+  //Copy the 2x3 transformation matrix into a 3x3 transformation matrix
+  cv::Mat H = cv::Mat::eye(3, 3, CV_64F);
+  for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 3; j++) {
+      H.at<double>(i, j) = r.at<double>(i, j);
+    }
+  }
+
+  double offsetX = 0.0, offsetY = 0.0, maxX = 0.0, maxY = 0.0;
+  //Get max offset outside of the image and max width / height
+  for(size_t i = 0; i < 4; i++) {
+    if(cornersTransform[i].x < offsetX) {
+      offsetX = cornersTransform[i].x;
+    }
+
+    if(cornersTransform[i].y < offsetY) {
+      offsetY = cornersTransform[i].y;
+    }
+
+    if(cornersTransform[i].x > maxX) {
+      maxX = cornersTransform[i].x;
+    }
+
+    if(cornersTransform[i].y > maxY) {
+      maxY = cornersTransform[i].y;
+    }
+  }
+
+  offsetX = -offsetX;
+  offsetY = -offsetY;
+  maxX += offsetX;
+  maxY += offsetY;
+
+  cv::Size size_warp(maxX, maxY);
+
+  //Create the transformation matrix to be able to have all the pixels
+  cv::Mat H2 = cv::Mat::eye(3, 3, CV_64F);
+  H2.at<double>(0,2) = offsetX;
+  H2.at<double>(1,2) = offsetY;
+
+  warpPerspective(src, dst, H2*H, size_warp);
+}
+
+
+
+
+
+
+
 static const std::string OPENCV_WINDOW = "Image window";
 
 class ImageConverter
@@ -114,7 +178,8 @@ class ImageConverter
      debug=cv::mean(blur_img);
      // meanStdDev(blur_img, Minav, minAV , CvArr** mask=NULL);
      
-     
+     rotate(blur_img, 180, blur_img);
+
       //equalizeHist( blur_img, binary );
       //imshow("histogram",binary);
       
@@ -326,14 +391,14 @@ if (maxVal-debug[0] > binary_tresh){
    some=arcLength( contours[i] , true );
    x[i]=mu[i].m10/mu[i].m00;
    y[i]=mu[i].m01/mu[i].m00;
-   double orientation = 0.5*atan(2*mu[i].m11 / (mu[i].m20 - mu[i].m02));
+   double orientation = 0.5*atan(2*mu[i].m11 / (mu[i].m20 - mu[i].m02)); //get say angle
 
    cout << "x        :" << x[i] << "========" << some <<endl;
    cout << "y        :" << y[i] << "========"<< some <<endl;
    cout << "Delta X  :" << x[i]-(640/2) << endl;
    cout << "Delta Y  :" << y[i]-(480/2) << endl;
 
-   cout << "Angle            :" << orientation <<  endl;
+   cout << "Angle (Pitch)           :" << orientation*57.3 <<  endl;
 
 
 
@@ -344,13 +409,12 @@ if (maxVal-debug[0] > binary_tresh){
 //else cout << "not found" << endl;
 
 
- } //for end
+ }                                          //for end
 
 
 
 
-}
-
+}                                           //end if
 
     //cv::imshow("ax asli", blur_img);
     //cv::imshow("binary", binary);
