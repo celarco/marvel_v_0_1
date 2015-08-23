885700aecb5410f6c78bf47fcd19b6312a65be6d
TIR4N4Z0R05 <mhkazemi_engineer@yahoo.com>
2015-08-23 07:45:03 -0400

    edit window detector package

--- src/window_detector.cpp
+++ src/window_detector.cpp
@@ -10,72 +10,17 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
-#include "opencv2/opencv.hpp"
-
 
 
 /**
- * this code under B.S.D licence
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
 
 
 
-// Return the rotation matrices for each rotation
-void rotate(cv::Mat& src, double angle, cv::Mat& dst) {
-  cv::Mat r = getRotationMatrix2D(cv::Point2f(), angle, 1.0);
 
-  //4 coordinates of the image
-  std::vector<cv::Point2f> corners(4);
-  corners[0] = cv::Point2f(0, 0);
-  corners[1] = cv::Point2f(0, src.rows);
-  corners[2] = cv::Point2f(src.cols, 0);
-  corners[3] = cv::Point2f(src.cols, src.rows);
 
-  std::vector<cv::Point2f> cornersTransform(4);
-  cv::transform(corners, cornersTransform, r);
 
-  //Copy the 2x3 transformation matrix into a 3x3 transformation matrix
-  cv::Mat H = cv::Mat::eye(3, 3, CV_64F);
-  for(int i = 0; i < 2; i++) {
-    for(int j = 0; j < 3; j++) {
-      H.at<double>(i, j) = r.at<double>(i, j);
-    }
-  }
-
-  double offsetX = 0.0, offsetY = 0.0, maxX = 0.0, maxY = 0.0;
-  //Get max offset outside of the image and max width / height
-  for(size_t i = 0; i < 4; i++) {
-    if(cornersTransform[i].x < offsetX) {
-      offsetX = cornersTransform[i].x;
-    }
-
-    if(cornersTransform[i].y < offsetY) {
-      offsetY = cornersTransform[i].y;
-    }
-
-    if(cornersTransform[i].x > maxX) {
-      maxX = cornersTransform[i].x;
-    }
-
-    if(cornersTransform[i].y > maxY) {
-      maxY = cornersTransform[i].y;
-    }
-  }
-
-  offsetX = -offsetX;
-  offsetY = -offsetY;
-  maxX += offsetX;
-  maxY += offsetY;
-
-  cv::Size size_warp(maxX, maxY);
-
-  //Create the transformation matrix to be able to have all the pixels
-  cv::Mat H2 = cv::Mat::eye(3, 3, CV_64F);
-  H2.at<double>(0,2) = offsetX;
-  H2.at<double>(1,2) = offsetY;
-
-  warpPerspective(src, dst, H2*H, size_warp);
-}
 
 
 
@@ -87,6 +32,8 @@ static const std::string OPENCV_WINDOW = "Image window";
 
 class ImageConverter
    {
+
+
      ros::NodeHandle nh_;
      image_transport::ImageTransport it_;
      image_transport::Subscriber image_sub_;
@@ -179,20 +126,9 @@ class ImageConverter
       
       blur(blur_img , binary , Size(3 , 3) , Point(-1,-1), 0 );
       
-//rotate function -----------------------------------------------------------------------------------------------------//
-
-
-       rotate(binary, 180, binary);
-
-
-//rotate function -----------------------------------------------------------------------------------------------------//
-
-
-
-
-      imshow ("blur shode",binary);
+  //    imshow ("blur shode",binary);
       
-      
+      /*
      cout << "maxval:   " << maxVal << endl ;
      cout << "minval:   " << minVal << endl;
      Minav=(maxVal+minVal)/2.0;
@@ -206,7 +142,7 @@ class ImageConverter
      
      cout << "================================" <<endl;
 
-
+*/
 
 
 
@@ -300,11 +236,21 @@ binary2=binary;
 
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+double x[10000];
+double y[10000];
 
  /// Get the moments
   vector<Moments> mu(contours.size() );
+
+
+
+
   for( int i = 0; i < contours.size(); i++ )
-     { mu[i] = moments( contours[i], false ); }
+     { mu[i] = moments( contours[i], false );
+
+
+  }
+
 
   ///  Get the mass centers:
   vector<Point2f> mc( contours.size() );
@@ -315,10 +261,15 @@ binary2=binary;
 
  createTrackbar( " binary_tresh:", "binary", &binary_tresh, max_thresh, 0 );
  
+
+
 if (maxVal-debug[0] > binary_tresh){
 	
   /// Draw contours
   Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+
+
+
   for( int i = 0; i< contours.size(); i++ )
      {
        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
@@ -327,31 +278,93 @@ if (maxVal-debug[0] > binary_tresh){
      
 
 
+
     
     /// Calculate the area with the moments 00 and compare with the result of the OpenCV function
-  printf("\t Info: Area and Contour Length \n");
+  //printf("\t Info: Area and Contour Length \n");
   for( int i = 0; i< contours.size(); i++ )
      {
-       printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+     //  printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
        circle( drawing, mc[i], 4, color, -1, 8, 0 );
+
      }
 
-
- imshow( "Contours", drawing );
+ imshow( "Origin Contour", drawing );
 
 
 
 }
 
 
+double some=0;
+
+
+if (maxVal-debug[0] > binary_tresh){
+
+
+ for( int i = 0; i < contours.size(); i++ )
+ {
+
+   if ( (arcLength( contours[i], true ) >= some) && (contourArea(contours[i])>= some) )
+     {
+
+       /// Draw contours
+       Mat drawing1 = Mat::zeros( canny_output.size(), CV_8UC3 );
+
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       drawContours( drawing1, contours, i, color, 2, 8, hierarchy, 0, Point() );
+
+       circle( drawing1, mc[i], 4, color, -1, 8, 0 );
+       imshow( "Target Lock", drawing1);
+
+   some=arcLength( contours[i] , true );
+   x[i]=mu[i].m10/mu[i].m00;
+   y[i]=mu[i].m01/mu[i].m00;
+   double orientation = 0.5*atan(2*mu[i].m11 / (mu[i].m20 - mu[i].m02));
+
+   cout <<"x       =" << x[i] << "========" << some <<endl;
+   cout <<"y       =" << y[i] << "========"<< some <<endl;
+   cout <<"Delta X   :" << x[i]-(640/2) << endl;
+   cout << "Delta Y  :" << y[i]-(480/2) << endl;
+
+   cout << "Angle            :" << orientation <<  endl;
+
+
+
+
+
+
+    }
+//else cout << "not found" << endl;
+
+
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 	
-    cv::imshow("Bax asli", blur_img);
+    //cv::imshow("ax asli", blur_img);
     //cv::imshow("binary", binary);
    
     
+    
+    
+    
 
+     
+     
      
 //    imshow( "Dilation Demo", dilation_dst );
 
@@ -378,10 +391,8 @@ if (maxVal-debug[0] > binary_tresh){
    printf( " *                                            * \n" );
    printf( " *     This program debug and Develop by      * \n" );
    printf( " *          Mohammad Hossein Kazemi           * \n" );
-   printf( " *                Ali Jameei                  * \n" );
    printf( " *        All Right reserved 2015-2016        * \n" );
    printf( " *      Email:Mhkazemi_engineer@yahoo.com     * \n" );
-   printf( " *       Email:Celarco.Group@Gmail.com        * \n" );
    printf( " *     AmirKabir University of Technology     * \n" );
    printf( " *    AUT-MAV AUTONUMUS AIRIAL VEHICLE TEAM   * \n" );
    printf( " *                                            * \n" );
